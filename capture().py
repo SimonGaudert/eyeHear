7cd38c9537e25953584c0768791064122b25dd7c
SimonGaudert <31967063+SimonGaudert@users.noreply.github.com>
2018-02-17 18:41:22 -0500

    Created capture() function which saves a webcam image to snapShot.png

---
+++ capture().py
@@ -0,0 +1,22 @@
+import cv2
+ 
+def capture():
+	#Camera 0 = webcam for laptop, this should be changed for USB camera
+	camera_port = 0
+	 
+	camera = cv2.VideoCapture(camera_port)#creates camera object using webcam index
+	 
+	def get_image():#gets single image
+	 retval, im = camera.read()
+	 return im
+	
+	ramp_frames = 30 #takes 30 frames for image calibration
+	for i in range(ramp_frames):
+		temp = get_image()
+	
+	camera_capture = get_image()
+	file = "/Users/simongaudert/Desktop/eyeHear/snapShot.png" #file path for the given snapShot
+	
+	cv2.imwrite(file, camera_capture)
+	
+	del(camera)#deletes camera in order to create another later
