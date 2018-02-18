c8f7f52f4d69864b1cbcd8aaa0e14d7ac940feab
NadaRadwan <radwanmohsenn@gmail.com>
2018-02-18 01:24:22 -0500

    retuning all values recognized by API in web and label recognition in an array to be used in an alg to compare for similarities and read only the similar ones

--- testAPI.py
+++ testAPI.py
@@ -11,42 +11,37 @@ import base64
 from getImage import capture
 from textToSpeech import speak
 
-def sendWebRequest(readType, image_url): 
+def sendWebRequest(image_url): 
     url = 'https://vision.googleapis.com/v1p1beta1/images:annotate?key=AIzaSyC5FQW1UL-jZ9xq7ibfh1ZcfCP_6G0XpUA'
-    if readType == "text":
-        type = "TEXT_DETECTION"
-    elif readType == "image":
-        type = "WEB_DETECTION"
-    elif readType == "label":
-        type = "LABEL_DETECTION"
+    types = ["WEB_DETECTION", "LABEL_DETECTION"]
+    v_aReadings=[]
 
-    data = '''{
-          "requests": [
-            {
-              "image": {
-                "content": "'''+ image_url + '''"
-              },
-              "features": [
+    # send request to google vision api to get WEB_DETECTION and LABEL_DETECTION
+    for type in types:
+        data = '''{
+              "requests": [
                 {
-                  "type": "'''+ type + '''"
+                  "image": {
+                    "content": "'''+ image_url + '''"
+                  },
+                  "features": [
+                    {
+                      "type": "'''+ type + '''"
+                    }
+                  ]
                 }
               ]
-            }
-          ]
-        }'''
+            }'''
+        response = requests.post(url, data=data)
 
-    print (data)
-    response = requests.post(url, data=data)
     if(response.ok):
         jData = json.loads(response.content)
-        # print (jData)
-        if readType == "text":
-            return (jData['responses'][0]['textAnnotations'][0]['description'])
-        elif readType == "image":
-            return (jData['responses'][0]['webDetection']['webEntities'][0]['description'])
-        elif readType == "label":
-            return (jData['responses'][0]['labelAnnotations'][0]['description']+ " and "+jData['responses'][0]['labelAnnotations'][2]['description'])
+        if type == "WEB_DETECTION":
+            v_aReadings+=(jData['responses'][0]['webDetection']['webEntities'])
+        elif type == "LABEL_DETECTION":
+            v_aReadings+=(jData['responses'][0]['labelAnnotations'])
 
+    return v_aReadings
 
 if __name__ == '__main__':
     capture()
@@ -56,6 +51,9 @@ if __name__ == '__main__':
         length = len(encoded_image_string)
 
         clean_string = encoded_image_string[2:(length-1)]
-        image_desc = sendWebRequest("label", clean_string)
-        print (image_desc)
-        speak("You are looking at "+ image_desc)
+        v_aReadings = sendWebRequest(clean_string)
+
+        for i in v_aReadings:
+            print (i['description'])
+        # print (image_desc)
+        # speak("You are looking at "+ image_desc)
